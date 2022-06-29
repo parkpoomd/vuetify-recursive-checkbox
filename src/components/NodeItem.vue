6255fba96bc21d106d2a13febd32b23f9fdf5d38
parkpoomd <parkpoom.d@gmail.com>
2022-06-29 13:58:04 +0700

    completed version 1

---
+++ src/components/NodeItem.vue
@@ -0,0 +1,94 @@
+<template>
+  <li>
+    <div class="d-flex align-center">
+      <button
+        type="button"
+        class="mr-2 d-inline-flex align-center"
+        v-if="hasChildren"
+        @click="handleClickParent"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          style="height: 24px; width: 24px; color: #bdbdbd"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
+
+      <div :class="`${!hasChildren ? 'pl-7' : ''}`">
+        <label>
+          <input
+            type="checkbox"
+            :checked="node.checked"
+            @input="handleClickNode(node, $event)"
+          />
+          <span class="ml-2">
+            {{ node.label }}
+          </span>
+        </label>
+      </div>
+    </div>
+
+    <ul
+      v-show="hasChildren && showChildren"
+      class="pl-6"
+      style="list-style: none"
+    >
+      <node-item
+        v-for="child in node.children"
+        :key="child.id"
+        :node="child"
+        :handleSelected="handleSelected"
+      ></node-item>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'NodeItem',
+
+  props: {
+    node: Object,
+    handleSelected: Function,
+  },
+
+  data() {
+    return {
+      showChildren: false,
+      isChecked: false,
+    };
+  },
+
+  computed: {
+    hasChildren() {
+      return this.node.children && this.node.children.length;
+    },
+  },
+
+  methods: {
+    handleClickParent() {
+      this.showChildren = !this.showChildren;
+    },
+    handleClickNode(node, event) {
+      this.isChecked = event.target.checked;
+      this.callRecursively(node);
+      this.handleSelected(node);
+    },
+    callRecursively(node) {
+      node.checked = this.isChecked;
+      if (node.children && node.children.length) {
+        node.children.forEach((childNode) => {
+          this.callRecursively(childNode);
+        });
+      }
+    },
+  },
+};
+</script>
